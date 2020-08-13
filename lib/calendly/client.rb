036ff12a79d045e518a80e91364c89c2237e2a26
Kenji Koshikawa <koshikawa2009@gmail.com>
2020-08-14 01:10:01 +0900

    - update comments by YARD format

--- lib/calendly/client.rb
+++ lib/calendly/client.rb
@@ -9,7 +9,7 @@ module Calendly
     API_HOST = 'https://api.calendly.com'
     AUTH_API_HOST = 'https://auth.calendly.com'
 
-    # @param token [String] a Calendly's access token.
+    # @param [String] token a Calendly's access token.
     def initialize(token = nil)
       @logger = Calendly.configuration.logger
       @token = token || Calendly.configuration.token
@@ -24,19 +24,15 @@ module Calendly
     # Get access token object
     #
     # @return [OAuth2::AccessToken]
-    #
+    # @since 0.0.1
     def access_token
       return @access_token if defined?(@access_token)
 
       config = Calendly.configuration
-      client = OAuth2::Client.new(
-        config.client_id,
-        config.client_secret,
-        client_options
-      )
+      client = OAuth2::Client.new(config.client_id,
+                                  config.client_secret, client_options)
       @access_token = OAuth2::AccessToken.new(
-        client,
-        @token,
+        client, @token,
         refresh_token: config.refresh_token,
         expires_at: config.token_expires_at
       )
@@ -54,7 +50,7 @@ module Calendly
     #
     # Get basic information about a user
     #
-    # @param uuid [String] User unique identifier, or the constant "me" to reference the caller
+    # @param [String] uuid User unique identifier, or the constant "me" to reference the caller
     # @return [Calendly::User]
     # @since 0.0.1
     def user(uuid = 'me')
@@ -64,35 +60,26 @@ module Calendly
 
     #
     # Returns all Event Types associated with a specified User.
-    # GET https://api.calendly.com/event_types
     #
-    # @param user_uri [String] View event types available for the specified user (user URI).
-    # @param options [Hash] the optional request parameters
-    # @param options :count [String] Number of rows to return.
-    # @param options :page_token [String] Pass this to get the next portion of collection.
-    # @param options :sort [String] Order results by the specified field and direction. Accepts comma-separated list of {field}:{direction} values.
-    # @return [event_types, next_params]
-    #  - event_types [Array<Calendly::EventType>]
-    #  - next_params [Hash]
-    #
+    # @param [String] user_uri View event types available for the specified user (user URI).
+    # @param [Hash] opts the optional request parameters.
+    # @option opts [Integer] :count Number of rows to return.
+    # @option opts [String] :page_token Pass this to get the next portion of collection.
+    # @option opts [String] :sort  Order results by the specified field and direction. Accepts comma-separated list of {field}:{direction} values.
+    # @return [Array<Array<Calendly::EventType>, Hash>]
+    #  - [Array<Calendly::EventType>] event_types
+    #  - [Hash] next_params the parameters to get next data. if thre is no next it returns nil.
     # @since 0.0.2
-    def event_types(user_uri, options = {})
+    def event_types(user_uri, opts = {})
       params = { user: user_uri }
-      if options.is_a? Hash
-        options.transform_keys!(&:to_sym)
-        params.merge!(sort: options[:sort]) if options[:sort]
-        params.merge!(count: options[:count]) if options[:count]
-        params.merge!(page_token: options[:page_token]) if options[:page_token]
+      if opts.is_a? Hash
+        opts.transform_keys!(&:to_sym)
+        params.merge!(opts.slice(:count, :page_token, :sort))
       end
       body = request :get, 'event_types', params
       items = body[:collection] || []
       ev_types = items.map { |item| EventType.new item }
-      if body[:pagination][:next_page]
-        next_page_query = URI.parse(body[:pagination][:next_page]).query
-        next_page_params = Hash[URI.decode_www_form(next_page_query)]
-        next_page_params.transform_keys!(&:to_sym)
-      end
-      [ev_types, next_page_params]
+      [ev_types, next_page_params(body)]
     end
 
     private
@@ -109,11 +96,8 @@ module Calendly
 
     def client_options
       faraday_build_proc = proc { |builder|
-        builder.response(
-          :json,
-          parser_options: { symbolize_names: true },
-          content_type: /\bjson$/
-        )
+        builder.response(:json, parser_options: { symbolize_names: true },
+                                content_type: /\bjson$/)
       }
       {
         site: API_HOST,
@@ -129,6 +113,15 @@ module Calendly
 
     def refresh!
       @access_token = access_token.refresh!
+    end
+
+    def next_page_params(body)
+      return unless body[:pagination]
+      return unless body[:pagination][:next_page]
+
+      uri = URI.parse body[:pagination][:next_page]
+      params = Hash[URI.decode_www_form(uri.query)]
+      params.transform_keys(&:to_sym)
     end
   end
 end
