cafefa9b57228ec4de50f8d6049f8d434237ab4e
Kenji Koshikawa <koshikawa2009@gmail.com>
2020-08-19 11:57:34 +0900

    remove an unused file

--- lib/calendly/models/organization.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Calendly
-  class Organization
-    include ModelUtils
-    API_URL = "#{Client::API_HOST}/organization/"
-    TIME_FIELDS = %i[created_at updated_at].freeze
-  end
-end
